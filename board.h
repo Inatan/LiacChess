c38dd97fcc945bfa9aeaee78ea5edb489d22a05a
Inatan Lopes Hertzog <inatan_hertzog@hotmail.com>
2014-04-30 13:19:58 -0300

    Definições pt 1.
    Objetivo é definir peças e jogadas

---
+++ board.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define SIZE 64
+#define TRUE 1
+#define FALSE !TRUE
+#define BRANCAS 1
+#define PRETAS -1 // RACISMO
+
+char board[SIZE];
+int 50moves;
+int winnner;
+int draw;
+int blackInfractions;
+int whiteInfractions;
+int whoMoves;
+int enpassant;
