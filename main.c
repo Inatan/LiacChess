f7b929fb98d0fabc0455ddf323c8571f085e56c5
Inatan Lopes Hertzog <inatan_hertzog@hotmail.com>
2014-06-05 20:50:37 -0300

    Conexão ao servidor local completa
    Funcionando agora partir para movimentos e minimax

--- main.c
+++ main.c
@@ -1,191 +1,180 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "board.h"
+#include <string.h>
+#include <windows.h>
+#include<winsock2.h>
 #include "cJSON.h"
-/*
-  Copyright (c) 2009 Dave Gamble
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-*/
-
-
-/* Parse text to JSON, then render back to text, and print! */
-void doit(char *text)
+int main ()
 {
-    char *out;
-    cJSON *json;
+    // long long int val=pow(10,64);
+    WSADATA wsa;
+    SOCKET s;
+    struct sockaddr_in server;
+    int tamanho;
 
-    json=cJSON_Parse(text);
-    if (!json)
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
     {
-        printf("Error before: [%s]\n",cJSON_GetErrorPtr());
-    }
-    else
-    {
-        out=cJSON_Print(json);
-        cJSON_Delete(json);
-        printf("%s\n",out);
-        free(out);
-    }
-}
-
-/* Read a file, parse, render back, etc. */
-void dofile(char *filename)
-{
-    FILE *f=fopen(filename,"rb");
-    fseek(f,0,SEEK_END);
-    long len=ftell(f);
-    fseek(f,0,SEEK_SET);
-    char *data=(char*)malloc(len+1);
-    fread(data,1,len,f);
-    fclose(f);
-    doit(data);
-    free(data);
-}
-
-/* Used by some code below as an example datatype. */
-struct record
-{
-    const char *precision;
-    double lat,lon;
-    const char *address,*city,*state,*zip,*country;
-};
-
-/* Create a bunch of objects as demonstration. */
-void create_objects()
-{
-    cJSON *root,*fmt,*img,*thm,*fld;
-    char *out;
-    int i;	/* declare a few. */
-
-    /* Here we construct some JSON standards, from the JSON site. */
-
-    /* Our "Video" datatype: */
-    root=cJSON_CreateObject();
-    cJSON_AddItemToObject(root, "name", cJSON_CreateString("Jack (\"Bee\") Nimble"));
-    cJSON_AddItemToObject(root, "format", fmt=cJSON_CreateObject());
-    cJSON_AddStringToObject(fmt,"type",		"rect");
-    cJSON_AddNumberToObject(fmt,"width",		1920);
-    cJSON_AddNumberToObject(fmt,"height",		1080);
-    cJSON_AddFalseToObject (fmt,"interlace");
-    cJSON_AddNumberToObject(fmt,"frame rate",	24);
-
-    out=cJSON_Print(root);
-    cJSON_Delete(root);
-    printf("%s\n",out);
-    free(out);	/* Print to text, Delete the cJSON, print it, release the string. */
-
-    /* Our "days of the week" array: */
-    const char *strings[7]= {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-    root=cJSON_CreateStringArray(strings,7);
-
-    out=cJSON_Print(root);
-    cJSON_Delete(root);
-    printf("%s\n",out);
-    free(out);
-
-    /* Our matrix: */
-    int numbers[3][3]= {{0,-1,0},{1,0,0},{0,0,1}};
-    root=cJSON_CreateArray();
-    for (i=0; i<3; i++) cJSON_AddItemToArray(root,cJSON_CreateIntArray(numbers[i],3));
-
-    /*	cJSON_ReplaceItemInArray(root,1,cJSON_CreateString("Replacement")); */
-
-    out=cJSON_Print(root);
-    cJSON_Delete(root);
-    printf("%s\n",out);
-    free(out);
-
-
-    /* Our "gallery" item: */
-    int ids[4]= {116,943,234,38793};
-    root=cJSON_CreateObject();
-    cJSON_AddItemToObject(root, "Image", img=cJSON_CreateObject());
-    cJSON_AddNumberToObject(img,"Width",800);
-    cJSON_AddNumberToObject(img,"Height",600);
-    cJSON_AddStringToObject(img,"Title","View from 15th Floor");
-    cJSON_AddItemToObject(img, "Thumbnail", thm=cJSON_CreateObject());
-    cJSON_AddStringToObject(thm, "Url", "http:/*www.example.com/image/481989943");
-    cJSON_AddNumberToObject(thm,"Height",125);
-    cJSON_AddStringToObject(thm,"Width","100");
-    cJSON_AddItemToObject(img,"IDs", cJSON_CreateIntArray(ids,4));
-
-    out=cJSON_Print(root);
-    cJSON_Delete(root);
-    printf("%s\n",out);
-    free(out);
-
-    /* Our array of "records": */
-    struct record fields[2]=
-    {
-        {"zip",37.7668,-1.223959e+2,"","SAN FRANCISCO","CA","94107","US"},
-        {"zip",37.371991,-1.22026e+2,"","SUNNYVALE","CA","94085","US"}
-    };
-
-    root=cJSON_CreateArray();
-    for (i=0; i<2; i++)
-    {
-        cJSON_AddItemToArray(root,fld=cJSON_CreateObject());
-        cJSON_AddStringToObject(fld, "precision", fields[i].precision);
-        cJSON_AddNumberToObject(fld, "Latitude", fields[i].lat);
-        cJSON_AddNumberToObject(fld, "Longitude", fields[i].lon);
-        cJSON_AddStringToObject(fld, "Address", fields[i].address);
-        cJSON_AddStringToObject(fld, "City", fields[i].city);
-        cJSON_AddStringToObject(fld, "State", fields[i].state);
-        cJSON_AddStringToObject(fld, "Zip", fields[i].zip);
-        cJSON_AddStringToObject(fld, "Country", fields[i].country);
+        printf("Failed. Error Code : %d",WSAGetLastError());
+        return 1;
     }
 
-    /*	cJSON_ReplaceItemInObject(cJSON_GetArrayItem(root,1),"City",cJSON_CreateIntArray(ids,4)); */
+    printf("Initialised.");
 
-    out=cJSON_Print(root);
-    cJSON_Delete(root);
-    printf("%s\n",out);
-    free(out);
+    if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+    {
+        printf("Could not create socket : %d" , WSAGetLastError());
+    }
 
-}
+    printf("Socket created.\n");
 
-int main (int argc, const char * argv[])
-{
-    /* a bunch of json: */
-    char text1[]="{\n\"name\": \"Jack (\\\"Bee\\\") Nimble\", \n\"format\": {\"type\":       \"rect\", \n\"width\":      1920, \n\"height\":     1080, \n\"interlace\":  false,\"frame rate\": 24\n}\n}";
-    char text2[]="[\"Sunday\", \"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]";
-    char text3[]="[\n    [0, -1, 0],\n    [1, 0, 0],\n    [0, 0, 1]\n	]\n";
-    char text4[]="{\n		\"Image\": {\n			\"Width\":  800,\n			\"Height\": 600,\n			\"Title\":  \"View from 15th Floor\",\n			\"Thumbnail\": {\n				\"Url\":    \"http:/*www.example.com/image/481989943\",\n				\"Height\": 125,\n				\"Width\":  \"100\"\n			},\n			\"IDs\": [116, 943, 234, 38793]\n		}\n	}";
-    char text5[]="[\n	 {\n	 \"precision\": \"zip\",\n	 \"Latitude\":  37.7668,\n	 \"Longitude\": -122.3959,\n	 \"Address\":   \"\",\n	 \"City\":      \"SAN FRANCISCO\",\n	 \"State\":     \"CA\",\n	 \"Zip\":       \"94107\",\n	 \"Country\":   \"US\"\n	 },\n	 {\n	 \"precision\": \"zip\",\n	 \"Latitude\":  37.371991,\n	 \"Longitude\": -122.026020,\n	 \"Address\":   \"\",\n	 \"City\":      \"SUNNYVALE\",\n	 \"State\":     \"CA\",\n	 \"Zip\":       \"94085\",\n	 \"Country\":   \"US\"\n	 }\n	 ]";
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 50100 );
 
-    /* Process each json textblock by parsing, then rebuilding: */
-    doit(text1);
-    doit(text2);
-    doit(text3);
-    doit(text4);
-    doit(text5);
+    //Connect to remote server
+    if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        puts("connect error");
+        return 1;
+    }
+    puts("Connected");
 
-    /* Parse standard testfiles: */
-    /*	dofile("../../tests/test1"); */
-    /*	dofile("../../tests/test2"); */
-    /*	dofile("../../tests/test3"); */
-    /*	dofile("../../tests/test4"); */
-    /*	dofile("../../tests/test5"); */
+    char *name,*cor,retorno[10000];
+    cJSON *entrada,*saida,*jogada;
+    entrada=cJSON_CreateObject();
+    cJSON_AddItemToObject(entrada, "name", cJSON_CreateString("Bobot"));
+    name=cJSON_PrintUnformatted(entrada);
+    puts(name);
 
-    /* Now some samplecode for building objects concisely: */
-    create_objects();
+    if( send(s , name , strlen(name) , 0) < 0)
+    {
+        puts("Send failed");
+        return 1;
+    }
+    puts("Data Send\n");
+    //Receive a reply from the server
+    if((tamanho = recv(s , retorno , 10000 , 0)) == SOCKET_ERROR)
+    {
+        puts("recv failed");
+        return 1;
+    }
+    retorno[tamanho]='\0';
+    puts("Reply received\n");
+    saida=cJSON_Parse(retorno);
+    name=cJSON_Print(saida);
+    puts(name);
+
+
+    if( send(s , name , strlen(name) , 0) < 0)
+    {
+        puts("Send failed");
+        return 1;
+    }
+    int from[2];
+    int to[2];
+    while(cJSON_GetObjectItem(saida,"winner")->valueint==0 && cJSON_GetObjectItem(saida,"draw")->valueint==0)
+    {
+        if( cJSON_GetObjectItem(saida,"who_moves")->valueint==1)
+        {
+            jogada= cJSON_CreateObject();
+            from[0]=0;
+            from[1]=0;
+            to[0]=0;
+            to[1]=1;
+            cJSON_AddItemToObject(jogada, "from", cJSON_CreateIntArray(from,2));
+            cJSON_AddItemToObject(jogada, "to", cJSON_CreateIntArray(to,2));
+            name=cJSON_Print(jogada);
+            if( send(s , name , strlen(name) , 0) < 0)
+            {
+                puts("Send failed");
+                return 1;
+            }
+            if((tamanho = recv(s , retorno , 10000 , 0)) == SOCKET_ERROR)
+            {
+                puts("recv failed");
+                return 1;
+            }
+            retorno[tamanho]='\0';
+            saida=cJSON_Parse(retorno);
+            name=cJSON_Print(saida);
+
+            from[0]=0;
+            from[1]=1;
+            to[0]=0;
+            to[1]=0;
+            jogada= cJSON_CreateObject();
+            cJSON_AddItemToObject(jogada, "from", cJSON_CreateIntArray(from,2));
+            cJSON_AddItemToObject(jogada, "to", cJSON_CreateIntArray(to,2));
+            name=cJSON_Print(jogada);
+            if( send(s , name , strlen(name) , 0) < 0)
+            {
+                puts("Send failed");
+                return 1;
+            }
+
+            if((tamanho = recv(s , retorno , 10000 , 0)) == SOCKET_ERROR)
+            {
+                puts("recv failed");
+                return 1;
+            }
+
+            retorno[tamanho]='\0';
+            saida=cJSON_Parse(retorno);
+            name=cJSON_Print(saida);
+
+        }
+        else
+        {
+            jogada= cJSON_CreateObject();
+            from[0]=7;
+            from[1]=0;
+            to[0]=7;
+            to[1]=1;
+            cJSON_AddItemToObject(jogada, "from", cJSON_CreateIntArray(from,2));
+            cJSON_AddItemToObject(jogada, "to", cJSON_CreateIntArray(to,2));
+            name=cJSON_Print(jogada);
+            if( send(s , name , strlen(name) , 0) < 0)
+            {
+                puts("Send failed");
+                return 1;
+            }
+            if((tamanho = recv(s , retorno , 10000 , 0)) == SOCKET_ERROR)
+            {
+                puts("recv failed");
+                return 1;
+            }
+            retorno[tamanho]='\0';
+            saida=cJSON_Parse(retorno);
+            name=cJSON_Print(saida);
+
+            from[0]=7;
+            from[1]=1;
+            to[0]=7;
+            to[1]=0;
+            jogada= cJSON_CreateObject();
+            cJSON_AddItemToObject(jogada, "from", cJSON_CreateIntArray(from,2));
+            cJSON_AddItemToObject(jogada, "to", cJSON_CreateIntArray(to,2));
+            name=cJSON_Print(jogada);
+            if( send(s , name , strlen(name) , 0) < 0)
+            {
+                puts("Send failed");
+                return 1;
+            }
+            if((tamanho = recv(s , retorno , 10000 , 0)) == SOCKET_ERROR)
+            {
+                puts("recv failed");
+                return 1;
+            }
+            retorno[tamanho]='\0';
+
+            saida=cJSON_Parse(retorno);
+            name=cJSON_Print(saida);
+        }
+    }
+    name=NULL;
 
     return 0;
 }
